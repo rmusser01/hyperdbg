9e9f9df615f921cb11bce43774374bd71a154663
roberto.paleari <roberto.paleari@4f445c4c-7cba-34e3-480f-c378df478542>
2010-05-27 19:26:59 +0000

    Copyright.

--- core/types.h
+++ core/types.h
@@ -1,3 +1,26 @@
+/*
+  Copyright notice
+  ================
+  
+  Copyright (C) 2010
+      Lorenzo  Martignoni <martignlo@gmail.com>
+      Roberto  Paleari    <roberto.paleari@gmail.com>
+      Aristide Fattori    <joystick@security.dico.unimi.it>
+  
+  This program is free software: you can redistribute it and/or modify it under
+  the terms of the GNU General Public License as published by the Free Software
+  Foundation, either version 3 of the License, or (at your option) any later
+  version.
+  
+  HyperDbg is distributed in the hope that it will be useful, but WITHOUT ANY
+  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+  A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License along with
+  this program. If not, see <http://www.gnu.org/licenses/>.
+  
+*/
+
 #ifndef _TYPES_H
 #define _TYPES_H
 
