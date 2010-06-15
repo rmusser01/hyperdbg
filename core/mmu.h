1574ed04fa7621e6e0372cb60542f6f22262a960
aristidefattori <aristidefattori@4f445c4c-7cba-34e3-480f-c378df478542>
2010-06-15 13:49:03 +0000

    PAE support.

--- core/mmu.h
+++ core/mmu.h
@@ -28,10 +28,42 @@
 #include "x86.h"
 
 /* Memory-management defines */
-#define PAGE_SIZE 0x1000
-#define LARGEPAGE_SIZE 0x400000
+#define PAGE_SIZE          0x1000
+#ifndef PAGE_ALIGN
+#define PAGE_ALIGN(addr) (((Bit32u) (addr)) & ~(PAGE_SIZE - 1))
+#endif
 
-#define CR3_TO_PDBASE(v) ((v) & 0xfffff000)
+#define PAGE_OFFSET(a) ((hvm_address) (a) - (hvm_address) PAGE_ALIGN(a))
+
+/* **** PAE **** */
+#ifdef ENABLE_PAE
+#define LARGEPAGE_SIZE 0x200000
+#define CR3_ALIGN(v) ((v) & 0xffffffe0)
+
+#define VA_TO_PDPTE(a) (((a) & 0xc0000000) >> 30)
+#define VA_TO_PDE(a)   (((a) & 0x3fe00000) >> 21)
+#define VA_TO_PTE(a)   (((a) & 0x001ff000) >> 12)
+
+#define PDE_TO_VA(a)   (((a) << 21) & 0x3fe00000)
+#define PTE_TO_VA(a)   (((a) << 12) & 0x001ff000)
+
+#define PHY_TO_FRAME(a)      ((Bit64u) (((a) >> 12) & 0xfffffffffffff))
+#define PHY_TO_LARGEFRAME(a) ((a) >> 21)
+
+#define FRAME_TO_PHY(a)      ((a) << 12)
+#define LARGEFRAME_TO_PHY(a) (((a)>>9) << 21)
+
+#define PDE_TO_VALID(a)  ((hvm_address) (a) & 0x1)
+
+#define LARGEPAGE_ALIGN(a) ((a) & ~((LARGEPAGE_SIZE)-1))
+#define LARGEPAGE_OFFSET(a) ((hvm_address) (a) - (hvm_address) LARGEPAGE_ALIGN(a))
+
+#define READ_PTE(a) *(Bit64u*) (a)
+
+#else
+/* **** Non-PAE **** */
+#define LARGEPAGE_SIZE     0x400000
+#define CR3_ALIGN(v) ((v) & 0xfffff000)
 
 #define VA_TO_PDE(a)   (((a) & 0xffc00000) >> 22)
 #define VA_TO_PTE(a)   (((a) & 0x003ff000) >> 12)
@@ -39,22 +71,20 @@
 #define PDE_TO_VA(a)   (((a) << 22) & 0xffc00000)
 #define PTE_TO_VA(a)   (((a) << 12) & 0x003ff000)
 
-#define PHY_TO_VA(a)   ((hvm_address) (a) + 0x80000000)
-#define PHY_TO_FRAME(a) ((Bit32u) (((a) >> 12) & 0xfffff))
-#define FRAME_TO_PHY(a) ((a) << 12)
+#define PHY_TO_FRAME(a)      ((Bit32u) (((a) >> 12) & 0xfffff))
 #define PHY_TO_LARGEFRAME(a) ((a) >> 22)
-#define LARGEFRAME_TO_PHY(a) ((a) << 22)
+
+#define FRAME_TO_PHY(a) ((a) << 12)
+#define LARGEFRAME_TO_PHY(a) (((a)>>10) << 22)
 
 #define PDE_TO_VALID(a)  ((hvm_address) (a) & 0x1)
 
 #define LARGEPAGE_ALIGN(a) ((a) & ~((LARGEPAGE_SIZE)-1))
-
-#ifndef PAGE_ALIGN
-#define PAGE_ALIGN(addr) (((Bit32u) (addr)) & ~(PAGE_SIZE - 1))
-#endif
-
-#define PAGE_OFFSET(a) ((hvm_address) (a) - (hvm_address) PAGE_ALIGN(a))
 #define LARGEPAGE_OFFSET(a) ((hvm_address) (a) - (hvm_address) LARGEPAGE_ALIGN(a))
+
+#define READ_PTE(a) *(Bit32u*) (a)
+
+#endif	/* ENABLE_PAE */
 
 hvm_status MmuMapPhysicalPage(hvm_phy_address phy, hvm_address* pva, PPTE poriginal);
 hvm_status MmuUnmapPhysicalPage(hvm_address va, PTE original);
